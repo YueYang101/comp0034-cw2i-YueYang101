8b62381fed113ed3dc2fdc128dafa2a955bd93d6
YueYang101 <zcemuex@ucl.ac.uk>
2024-04-05 12:32:12 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # comp0034-cw2i-YueYang101
+## Setup
+
+Follow these steps to set up the project:
+
+```bash
+# Create a virtual environment
+# MacOS
+python3 -m venv .venv
+# Windows
+py -m venv .venv
+
+# Activate the virtual environment
+# MacOS
+source .venv/bin/activate
+# Windows
+.venv\Scripts\activate
+
+# Install the requirements
+pip install -r requirements.txt
+
+# Run the app with command
+flask --app paralympics run --debug
+
+# Stop the app by pressing CTRL+C
+
+# Check if the instance folder created which contains paralympics.sqlite
+
+# Using following input to access data
+# /total_sales_each_brand/<specific_date>
+# /promo_record_each_brand/<specific_date>
+# Date format example: 2024-01-02
+
+
+# read the pdf file 'comp0034-coursework1'
--- tests/app_test.py
+++ tests/app_test.py
@@ -13,6 +13,8 @@ os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.realpath(__file__))
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
 # create the path to the chromedriver
+# The chromedriver version is 123.0.6312.105 version win 64 platform.
+# The version of the chromedriver should be compatible with the version of the Chrome browser installed on your machine.
 path_to_chromedriver = os.path.join(current_directory,'chromedriver.exe')  # add .exe for Windows
 
 # create a new Chrome browser instance
@@ -21,7 +23,7 @@ driver = webdriver.Chrome(service=s)
 
 
 
-def test_server_live(dash_duo):
+def test_1_server_live(dash_duo):
     # Create the app
     app = import_app(app_file="PastaSales_dash")
     # Start the server with the app using the dash_duo fixture
@@ -31,7 +33,6 @@ def test_server_live(dash_duo):
     dash_duo.driver.implicitly_wait(2)
 
     # Get the url for the web app root
-    # You can print this to see what it is e.g. print(f'The server url is {url}')
     url = dash_duo.driver.current_url
 
     # Requests is a python library and here is used to make a HTTP request to the sever url
@@ -42,7 +43,7 @@ def test_server_live(dash_duo):
 
     
 
-def test_home_h1textequals(dash_duo):
+def test_2_home_h1textequals(dash_duo):
 
     """
     GIVEN the app is running
@@ -63,7 +64,7 @@ def test_home_h1textequals(dash_duo):
 
 
 
-def test_element_exists(dash_duo):
+def test_3_bar_chart_exists(dash_duo):
     app = import_app(app_file="PastaSales_dash")
     dash_duo.start_server(app)
 
@@ -74,8 +75,29 @@ def test_element_exists(dash_duo):
     assert dash_duo.find_element("#bar") is not None
 
 
+def test_4_line_chart_1_exists(dash_duo):
+    app = import_app(app_file="PastaSales_dash")
+    dash_duo.start_server(app)
 
-def test_button_click_changes_page(dash_duo):
+    # Wait for the specific element to be visible, timeout if this does not happen within 20 seconds
+    dash_duo.wait_for_element("#line_1", timeout=20)
+
+    # Check if the element exists
+    assert dash_duo.find_element("#line_1") is not None
+
+
+def test_5_line_chart_2_exists(dash_duo):
+    app = import_app(app_file="PastaSales_dash")
+    dash_duo.start_server(app)
+
+    # Wait for the specific element to be visible, timeout if this does not happen within 20 seconds
+    dash_duo.wait_for_element("#line_2", timeout=20)
+
+    # Check if the element exists
+    assert dash_duo.find_element("#line_2") is not None
+
+
+def test_6_button_click_changes_page(dash_duo):
     app = import_app(app_file="PastaSales_dash")
     dash_duo.start_server(app)
 
@@ -93,7 +115,7 @@ def test_button_click_changes_page(dash_duo):
 
 
 
-def test_dropdown_has_correct_options(dash_duo):
+def test_7_dropdown_has_correct_options(dash_duo):
     app = import_app(app_file="PastaSales_dash")
     dash_duo.start_server(app)
 
@@ -102,7 +124,7 @@ def test_dropdown_has_correct_options(dash_duo):
 
     # Get the dropdown options
     dropdown = dash_duo.find_element("#type-dropdown_1")
-    options = [option.text for option in dropdown.find_elements_by_tag_name("option")]
+    options = [option.text for option in dropdown.find_elements_by_tag_name("option") if option.text.strip() != ""]
 
     # Check if the options are correct
     assert options == ["Brand 1", "Brand 2", "Brand 3", "Brand 4"]
